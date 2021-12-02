0766403c1b4eaa845464c4d633cc728494fa75b2
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-12-02 13:37:39 +0100

    readme

---
+++ patuco/variables.js
@@ -0,0 +1,29 @@
+const variables = {
+         "fonts": [
+"@import url('https://fonts.googleapis.com/css2?family=Spartan:wght@300&display=swap')",
+"@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@800&display=swap')"
+],
+   "primaryfontFamily1": "Spartan",
+   "primaryfontFamily2": "Spartan",
+   "primaryfontFamily3": "Spartan",
+   "primaryfontFamily4": "Spartan",
+   "primaryfontFamily5": "Spartan",
+   "secundaryfontFamily1": "Spartan",
+   "secundaryfontFamily2": "Spartan",
+   "secundaryfontFamily3": "Spartan",
+   "secundaryfontFamily4": "Spartan",
+   "secundaryfontFamily5": "Spartan",
+   "primary-color1": "#666569",
+   "color-icon": "#8b8b8b",
+   "dark-grey": "#929292",
+   "ligth-grey": "#c8c7c7",
+   "color-white": "white",
+   "color-red": "#de5246",
+   "background-colorBody": "#e9eaea",
+   "background-color1": "#e9eaea",
+   "background-color2": "#e9eaea",
+   "container": "0 9% 0 9%",
+};
+  
+module.exports = variables;
+  
